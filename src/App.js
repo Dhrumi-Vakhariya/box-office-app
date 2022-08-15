b039ce71cf80985991ed1ce9263a676cc8d373d2
Dhrumi Vakhariya <dhrumi243@gmail.com>
2022-08-15 14:56:00 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          This is a home page!
+        </Route>
+        <Route exact path="/starred">
+          This is a starred page!
+        </Route>
+        <Route>Sorry, the page you visited does not exist.</Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
