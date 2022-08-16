f94259791fd5dc302bdcae54f626e8d056b7d217
Dhrumi Vakhariya <dhrumi243@gmail.com>
2022-08-16 15:40:18 +0530

    Implemented search button and integrated TV MAZE API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import MainLayout from "../components/MainLayout";
 
 const Home = () => {
-  return <MainLayout>Welcome To Home Page!</MainLayout>;
+  const [input, setinput] = useState("");
+
+  const onInputChange = ev => {
+    setinput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(res =>
+      res.json().then(result => {
+        console.log("first", result);
+      })
+    );
+  };
+
+  const onKeyDown = ev => {
+    console.log("not 13");
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        value={input}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainLayout>
+  );
 };
 
 export default Home;
